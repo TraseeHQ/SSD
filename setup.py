6a2e9bb5c3abfbd8f75b4fc171d130f253647d0f
Bartłomiej Kurzeja <bartlomiej+gitlab@kurzeja.it>
2019-06-24 22:40:52 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ssd',
+    version='0.0.0',
+    packages=find_packages(include=['ssd.*']),
+    install_requires=[
+        'torch~=1.0',
+        'torchvision~=0.3',
+        'opencv-python~=4.0',
+        'yacs==0.1.6'
+    ]
+)
